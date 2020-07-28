f9660a137eb63c574467cc6154135236f069eeb4
Yan <yanpoissondube@gmail.com>
2020-07-28 11:43:32 -0400

    Bonjour

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,14 +1,2 @@
-import random
-
-nombre_m = random.randint(0, 10)
-nombre = input("Quel est le nombre mystère ? ")
-if nombre.isdigit():
-    nombre = int(nombre)
-    if nombre > nombre_m:
-        print(f"Le nombre mystère est plus petit que {nombre}")
-    elif nombre < nombre_m:
-        print(f"Le nombre mystère est plus grand que {nombre}")
-    else:
-        print("Bravo, vous avez trouvé le nombre mystère !")
-else:
-    print("SVP, entrez un nombre valide.")    
+for num in range(1,10 + 1):
+    print(num)
